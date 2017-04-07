53d4df3b382a08480705d258d2048da4483948c3
Rob O'Donnell <r.odonnell@farmlead.com>
2017-04-07 11:20:25 -0400

    smooth scroll button

--- _site/js/main.js
+++ _site/js/main.js
@@ -14,3 +14,18 @@ function closeNav() {
 $way.waypoint(function () {
   $nav.toggleClass('bg-navy');
 });
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
