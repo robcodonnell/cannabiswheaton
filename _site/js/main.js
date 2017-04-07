655bb343b595236ed1311eb0c5840c46647d8319
Rob O'Donnell <r.odonnell@farmlead.com>
2017-04-06 20:59:15 -0400

    secure links for images and videos

--- _site/js/main.js
+++ _site/js/main.js
@@ -3,10 +3,12 @@ var $nav = $('nav');
 
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
+  document.getElementById("myNav").style.opacity = "1";
 };
 
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myNav").style.opacity = "0";
 };
 
 $way.waypoint(function () {
