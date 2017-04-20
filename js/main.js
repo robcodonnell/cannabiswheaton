5b107d14ad680e12dde1172065e6d404ae4c0e9c
Rob O'Donnell <r.odonnell@farmlead.com>
2017-04-20 17:06:09 -0400

    replace placeholder gif

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//nav
 var $way = $('.way');
 var $nav = $('nav');
 
@@ -15,6 +16,7 @@ $way.waypoint(function () {
   $nav.toggleClass('bg-navy');
 });
 
+//page builds
 $('.para-box').waypoint(function () {
   $('.para-box').css('opacity', 1);
 },
@@ -25,6 +27,7 @@ $('.team-profiles').waypoint(function () {
 },
 { offset: '50%' });
 
+//smooth-scroll arrow
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -40,6 +43,47 @@ $(function() {
   });
 });
 
+//gifs
 $(document).ready(function () {
-  $(".gif").attr("src", "https://res.cloudinary.com/dtbrkt2ur/image/upload/v1492718034/leaf_juc8zd.gif?" + Math.random());
+  $(".gif-one").attr("src", "https://res.cloudinary.com/dtbrkt2ur/image/upload/v1492722194/future-positioning_m7ghp1.gif?" + Math.random());
 });
+
+$(document).ready(function () {
+  $(".gif-two").attr("src", "https://res.cloudinary.com/dtbrkt2ur/image/upload/v1492722194/future-positioning_m7ghp1.gif?" + Math.random());
+});
+
+$(document).ready(function () {
+  $(".gif-three").attr("src", "https://res.cloudinary.com/dtbrkt2ur/image/upload/v1492722194/future-positioning_m7ghp1.gif?" + Math.random());
+});
+
+$(document).ready(function () {
+  $(".gif-four").attr("src", "https://res.cloudinary.com/dtbrkt2ur/image/upload/v1492722194/future-positioning_m7ghp1.gif?" + Math.random());
+});
+
+$('.grid-one').waypoint(function () {
+  $('.gif-one').css({
+    'display' : 'block'
+});
+},
+{ offset: '70%' });
+
+$('.grid-two').waypoint(function () {
+  $('.gif-two').css({
+    'display' : 'block'
+});
+},
+{ offset: '70%' });
+
+$('.grid-three').waypoint(function () {
+  $('.gif-three').css({
+    'display' : 'block'
+});
+},
+{ offset: '70%' });
+
+$('.grid-four').waypoint(function () {
+  $('.gif-four').css({
+    'display' : 'block'
+});
+},
+{ offset: '70%' });
